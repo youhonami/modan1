d0e64a85df854adb627b087b879f840e0ed99912
youhonami <youhonami42@gmail.com>
2025-07-16 04:54:18 +0900

    Remove accidentally added modan1.git repo

--- modan11/nuxt.config.ts
+++ modan11/nuxt.config.ts
@@ -4,4 +4,17 @@ export default defineNuxtConfig({
   css: ["@/assets/css/form.css"],
   modules: ["@nuxtjs/tailwindcss"],
   plugins: ["~/plugins/firebase.ts"],
+
+  runtimeConfig: {
+    public: {
+      firebaseApiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId:
+        process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
+      firebaseMeasurementId: process.env.NUXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    },
+  },
 });
